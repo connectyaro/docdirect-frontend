bb5fe262c3b30ec2787da5fb6b9d0f589ba06328
Yasin Hirani <yasin.hirani@1rivet.com>
2024-06-10 12:57:54 +0530

    Fix : Routing

--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -1,40 +1,37 @@
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import App from "../App";
+import ProtectedRoutes from "./ProtectedRoutes";
+import Admin from "../pages/admin/Admin";
+import Login from "../core/components/auth/Login";
+import Register from "../core/components/auth/Register";
+import Business from "../pages/business/Business";
+import User from "../pages/user/User";
+import Home from "../pages/home/Home";
 
 const Router = () => {
-  const isAuthenticated = false;
-  // eslint-disable-next-line prefer-const
-  let role = "user";
   const routes = createBrowserRouter([
-    !isAuthenticated
-      ? {
-          path: "/",
-          element: <div>Landing Page</div>,
-        }
-      : {
-          path: "/",
-          element: <App />,
-          children: [
-            role === "user"
-              ? {
-                  path: "/",
-                  element: <div>User</div>,
-                }
-              : {},
-            role === "doctor"
-              ? {
-                  path: "/",
-                  element: <div>Doctor</div>,
-                }
-              : {},
-            role === "admin"
-              ? {
-                  path: "/",
-                  element: <div>Admin</div>,
-                }
-              : {},
-          ],
+    {
+      path: "/home",
+      element: <ProtectedRoutes Children={Home} />,
+    },
+    {
+      path: "/",
+      element: <ProtectedRoutes Children={App} />,
+      children: [
+        {
+          path: "/clinic-list",
+          element: <User />,
         },
+      ],
+    },
+    {
+      path: "/doctor",
+      element: <ProtectedRoutes Children={Business} />,
+    },
+    {
+      path: "/admin",
+      element: <ProtectedRoutes Children={Admin} />,
+    },
     {
       path: "/about-us",
       element: <div>About us</div>,
@@ -42,6 +39,14 @@ const Router = () => {
     {
       path: "/contact-us",
       element: <div>Contact us</div>,
+    },
+    {
+      path: "/login",
+      element: <ProtectedRoutes Children={Login} />,
+    },
+    {
+      path: "/signup",
+      element: <ProtectedRoutes Children={Register} />,
     },
     {
       path: "*",
